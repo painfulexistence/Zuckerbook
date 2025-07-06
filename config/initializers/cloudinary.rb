79194a614dd624b2e2baf5fb5c3692cd4472ce20
loicchen <910041@gmail.com>
2025-07-06 18:17:46 +0800

    chore: migrate Cloudinary settings

--- config/initializers/cloudinary.rb
+++
@@ -1,7 +0,0 @@
-Cloudinary.config do |config|
-  config.cloud_name = 'dx2v1qk7g'
-  config.api_key = '159786931271326'
-  config.api_secret = 'u3GkvjYCdRLv8QJ7i_tBY1QeKYo'
-  config.secure = true
-  config.cdn_subdomain = true
-end
