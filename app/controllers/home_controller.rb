8321766d685263e0cd877f55f0ff13c32859702a
loicchen <910041@gmail.com>
2025-07-07 10:06:16 +0800

    feat: add home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,6 @@
 class HomeController < ApplicationController
+  layout false
+
   def index
-    if user_signed_in?
-      redirect_to posts_path
-    else
-      @posts = Post.includes(:user).where(public: true).order('updated_at DESC')
-    end
   end
 end
